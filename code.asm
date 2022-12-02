cccc995989da8752d279de008bc431700a1ac783
Desmon <61366533+desmonHak@users.noreply.github.com>
2022-12-02 21:02:55 +0100

    21:02:47,90

--- code.asm
+++ code.asm
@@ -1,1 +1,6 @@
-[org 0x01]
+bits 16
+
+org 0x01   ; no es una instrucion org = origen
+           ; Sino una directiva, esta le indica
+           ; a nasm, que el codigo de aqui en adelante
+           ; se aloje apartir de dicha direcion
